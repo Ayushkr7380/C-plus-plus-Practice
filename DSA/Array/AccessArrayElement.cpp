cc060ea8b32f133addf901ecfaf325d985fd57c3
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-03-25 23:12:03 +0530

    AccessArrayElement code added

---
+++ DSA/Array/AccessArrayElement.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[] = {12,3,45,3,23};
+
+    //Accessing 3rd element
+    cout << arr[2] <<endl;
+
+    //Acccessing 1st element
+    cout << arr[0];
+
+    
+    return 0;
+}
