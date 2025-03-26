79da19febec42700c396565c0b5bef304c4d4b36
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-03-26 20:27:10 +0530

    traversingArray code added

---
+++ DSA/Array/traversingArray.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[] = {12,434,3,4,56};
+
+    //Traversing and print array
+    for(int i=0;i<5;i++){
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
