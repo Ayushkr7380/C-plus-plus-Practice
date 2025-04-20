9ab10ce5055927fac194befc06b35fdf4d34278a
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-04-21 01:55:12 +0530

    practice set 01 oops c++

---
+++ College/Practice_Set_OOPs/13-04-2025/BankAccoutClass.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class BankAccout{
+    public:
+        int accountNumber;
+        string accountHolderName;
+        float balance;
+
+        BankAccout(int accountNumber,string accountHolderName,float balance){
+            this->accountNumber = accountNumber;
+            this->accountHolderName = accountHolderName;
+            this->balance = balance;
+        }
+
+        void display(){
+            cout<<"Account Number : "<<this->accountNumber<<endl;
+            cout<<"accountHolderName : "<<this->accountHolderName<<endl;
+            cout<<"Balance : "<<this->balance<<endl;
+        }
+};
+int main(){
+    BankAccout b1(1,"Ayush 1",122.4);
+    b1.display();
+    BankAccout b2(2,"Ayush 2",222.4);
+    b2.display();
+    BankAccout b3(3,"Ayush 3",322.4);
+    b3.display();
+    return 0;
+}
