1e86831cce4d625f8e45f4fb98315db63f21e5e3
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-05-07 23:09:22 +0530

    comment added

--- College/06-05-2025/MultipleInheritance.cpp
+++ College/06-05-2025/MultipleInheritance.cpp
@@ -1,31 +1,39 @@
 #include<iostream>
 using namespace std;
 
+// Base class 1
 class Genius {
 public:
+    // Method to simulate high intelligence
     void brain() {
         cout << "Uses high IQ" << endl;
     }
 };
 
+// Base class 2
 class Billionaire {
 public:
+    // Method to simulate having wealth
     void money() {
         cout << "Has lots of money" << endl;
     }
 };
 
+// Derived class inheriting from both Genius and Billionaire
 class IronMan : public Genius, public Billionaire {
 public:
+    // Method specific to IronMan
     void show() {
         cout << "Iron Man is a genius billionaire" << endl;
     }
 };
 
 int main() {
-    IronMan i;
-    i.show();
-    i.brain();
-    i.money();
+    IronMan i;       // Create object of derived class IronMan
+
+    i.show();        // Call method specific to IronMan
+    i.brain();       // Call inherited method from Genius
+    i.money();       // Call inherited method from Billionaire
+
     return 0;
 }
